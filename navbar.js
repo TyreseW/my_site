65d6ed5f38a76f574402495890f512a767101aa8
TyreseW <96143128+TyreseW@users.noreply.github.com>
2024-11-04 22:24:04 -0500

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,16 @@
+//Upon clicking Assignments, display the assignment list
+var dropdown = document.getElementsByClassName("dropdownBtn");
+var i;
+
+for (let i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });    
+}
+ 
